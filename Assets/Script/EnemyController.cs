739b5d7cc45a179ca67f4b700e1f1c777b5734a1
LeeChangHyeon <tryeffort46@gmail.com>
2026-02-04 13:22:51 +0900

    enemycontroller 구현
    Cylinder 추가

---
+++ Assets/Script/EnemyController.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("EnemyController");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
