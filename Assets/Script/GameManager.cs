7432ccc447ea473f237648d32ebf947bb63a190a
LeeChangHyeon <tryeffort46@gmail.com>
2026-02-04 11:47:30 +0900

    GameManager 수정
    주석추가

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -5,6 +5,7 @@ public class GameManager : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        // 주석
         Debug.Log("GameManager has started");
     }
 
